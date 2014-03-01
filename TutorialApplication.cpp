d5dee99a4dbefa54000d297f3ffc73512f2c951f
Fernanda Bernardo <fernandabernardo94@gmail.com>
2014-03-01 13:18:25 -0300

    adicionando outra entity

--- TutorialApplication.cpp
+++ TutorialApplication.cpp
@@ -26,19 +26,19 @@ TutorialApplication::~TutorialApplication(void)
 }
 
 //-------------------------------------------------------------------------------------
-void TutorialApplication::createScene(void)
-{
-   // Set the scene's ambient light
+void TutorialApplication::createScene(void) {
     mSceneMgr->setAmbientLight(Ogre::ColourValue(0.5f, 0.5f, 0.5f));
 
-    // Create an Entity
     Ogre::Entity* ogreHead = mSceneMgr->createEntity("Head", "ogrehead.mesh");
 
-    // Create a SceneNode and attach the Entity to it
     Ogre::SceneNode* headNode = mSceneMgr->getRootSceneNode()->createChildSceneNode("HeadNode");
     headNode->attachObject(ogreHead);
 
-    // Create a Light and set its position
+    Ogre::Entity* ogreHead2 = mSceneMgr->createEntity("Head2", "ogrehead.mesh");
+
+    Ogre::SceneNode* headNode2 = mSceneMgr->getRootSceneNode()->createChildSceneNode("HeadNode2", Ogre::Vector3(100,0,0));
+    headNode2->attachObject(ogreHead2);
+
     Ogre::Light* light = mSceneMgr->createLight("MainLight");
     light->setPosition(20.0f, 80.0f, 50.0f);
 }
